2d77e4ee71e7fca0f8e605223027c4957a59dd4a
PC PRO ULTRA RAPIDE <anassdah5@gmail.com>
2021-05-24 00:55:45 +0100

    added menu to StationIMagesActivity commit

--- app/src/main/java/com/example/myapplication/StationImagesActivity.kt
+++ app/src/main/java/com/example/myapplication/StationImagesActivity.kt
@@ -7,6 +7,9 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.GridLayoutManager
@@ -92,5 +95,35 @@ class StationImagesActivity : AppCompatActivity(), OnitemClcikListerner {
         intent.putExtra("pos", position)
         intent.putExtra("imageList", myimageFile)
         startActivity(intent)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inf : MenuInflater = menuInflater
+        inf.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id : Int = item.itemId
+        if (id==R.id.menu_home){
+            Toast.makeText(this,"Home",Toast.LENGTH_SHORT).show()
+            val intent:Intent=Intent(this,HActivity::class.java)
+            startActivity(intent)
+        }
+        if (id==R.id.menu_map){
+            Toast.makeText(this,"Map",Toast.LENGTH_SHORT).show()
+            val intent:Intent=Intent(this,MapActivity::class.java)
+            startActivity(intent)
+        }
+        if (id==R.id.setting_language){
+            Toast.makeText(this,"Language",Toast.LENGTH_SHORT).show()
+            val intent:Intent=Intent(this,LanguageActivity::class.java)
+            startActivity(intent)
+        }
+        if (id==R.id.setting_theme){
+            Toast.makeText(this,"Theme",Toast.LENGTH_SHORT).show()
+            val intent:Intent=Intent(this,ThemeActivity::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
